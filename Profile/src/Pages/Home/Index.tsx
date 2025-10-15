7886d55d4dddc28299cf607fa55bbcf3c9803b84
Yugen1121 <yugenlimbu777@gmail.com>
2025-10-15 14:00:04 +0100

    Implemented the nav bar

--- Profile/src/Pages/Home/Index.tsx
+++ Profile/src/Pages/Home/Index.tsx
@@ -1,34 +1,28 @@
 import { DyClass } from "../../Helper/DyClass";
 import "../../App.css"
 import gsap from "gsap/all"
-import ModifiersPlugin from "gsap/all"
-import Whale from "../../assets/FloatingWhale.png"
-import Koi from "../../assets/TwoKoi.png"
-import Sun from "../../assets/Sun.png"
-import Cat from "../../assets/PeakingCat.png"
 import { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import {Button} from "@mui/material"
+import NavBar from "./NavBar"
+import Introduction from "./Introduction";
+import AboutMe from "./AboutMe";
 
 function Home(){
+    // NavBar
+    const Nav = {
+        "About": "About",
+        "Projects": "Projects",
+        "Skills": "Skills"
+    }
 
     return <>
-    <div>
-        Yugen Limbu
+    <NavBar Sections={Nav} />
+    <div id="Home" className="ScreenSize flex justify-center flex-col items-center">
+
+        <Introduction />
+        
     </div>
-    <div>
-        Software Developer | CS student 
-    </div>
-    <div>
-        <Button variant="outlined">
-            LinkedIn
-        </Button>
-        <Button variant="outlined">
-            GitHub
-        </Button>
-        <Button variant="outlined">
-            Gmail
-        </Button>
+    <div id="About" className="ScreenSize Center">
     </div>
     </>
 }
