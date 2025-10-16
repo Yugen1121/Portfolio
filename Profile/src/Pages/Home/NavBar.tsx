9a156be2aa186f199379ef67c40136507b85ee9c
Yugen1121 <yugenlimbu777@gmail.com>
2025-10-16 11:52:59 +0100

    Changed Nav bar

--- Profile/src/Pages/Home/NavBar.tsx
+++ Profile/src/Pages/Home/NavBar.tsx
@@ -1,31 +1,28 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import Divider from '@mui/material/Divider';
-import Drawer from '@mui/material/Drawer';
-import IconButton from '@mui/material/IconButton';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-
-interface Props {
-  /**
-   * Injected by the documentation to work in an iframe.
-   * You won't need it on your project.
-   */
-  window?: () => Window;
-}
-
+import { DyClass } from "../../Helper/DyClass";
+import { useEffect, useState } from "react";
+import "../../App.css"
 
 const drawerWidth = 240;
 
 function NavBar({Sections}: {Sections: Record<string, string>}) {
 
+    
+
+    const [width, updateWidth] = useState(window.innerWidth)
+
+    
+
+    useEffect(()=>{
+        const onWidthChange = ()=>{
+        updateWidth(window.innerWidth);
+    }
+
+        window.addEventListener("resize", onWidthChange);
+        return ()=> window.removeEventListener("resize", onWidthChange)
+    },[])
+    
+
+    // Used to nav through the page
     const onClickNav = (id: string)=>{
         const section = document.getElementById(id);
         if (section){
@@ -33,89 +30,35 @@ function NavBar({Sections}: {Sections: Record<string, string>}) {
         }
     }
     
-    const [mobileOpen, setMobileOpen] = React.useState(false);
+    
+    const drawer = <>
+        <div className={
+            DyClass("h-screen fixed")
+        }>
 
-    const handleDrawerToggle = () => {
-    setMobileOpen((prevState) => !prevState);
-    };
-
-    const drawer = (
-    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
-        <Typography variant="h6" sx={{ my: 2 }}>
-        Menue
-        </Typography>
-        <Divider />
-        <List>
-        {Object.keys(Sections).map((item) => (
-            <ListItem key={item} disablePadding>
-            <ListItemButton key={item} onClick={()=>onClickNav(item)} sx={{ textAlign: 'center' }}>
-                <ListItemText primary={Sections[item]} />
-            </ListItemButton>
-            </ListItem>
-        ))}
-        </List>
-    </Box>
-    );
-
-    const container = undefined;
-
-    return (
-    <div className='fixed z-1000'>
-        <Box sx={{ display: 'flex',  }}>
-        <CssBaseline />
-        <AppBar component="nav">
-            <Toolbar>
-            
-                <Typography
-                    onClick={()=>onClickNav("Home")}
-                    variant="h6"
-                    component="div"
-                    sx={{ flexGrow: 1, display: {sm: 'block' } }}
-                >
-                    YL
-                </Typography>
-                <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
-                    {Object.keys(Sections).map((item) => (
-                    <Button key={item} onClick={()=>onClickNav(item)} sx={{ color: '#fff' }}>
-                        {Sections[item]}
-                    </Button>
-                    ))}
-                </Box>
-                <IconButton
-                color="inherit"
-                aria-label="open drawer"
-                edge="end"
-                onClick={handleDrawerToggle}
-                sx={{ mr: 2, display: { sm: 'none' } }}
-                >
-                Yugen
-                </IconButton>
-            </Toolbar>
-        </AppBar>
-        <nav>
-            <Drawer
-            anchor='right'
-            container={container}
-            variant="temporary"
-            open={mobileOpen}
-            onClose={handleDrawerToggle}
-            ModalProps={{
-                keepMounted: true, // Better open performance on mobile.
-            }}
-            sx={{
-                display: { xs: 'block', sm: 'none' },
-                '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-            }}
-            >
-            {drawer}
-            </Drawer>
-        </nav>
-        <Box component="main" sx={{ p: 3 }}>
-            <Toolbar />
-        </Box>
-        </Box>
         </div>
-    );
+    </>
+    
+    return <>
+    <div className="flex w-screen justify-end fixed z-1000">
+        <div className={
+            DyClass(((width<640)? "hidden": "flex"), "p-[8px] pl-[12px]")
+        }>
+            {Object.keys(Sections).map((i)=>(
+                <button  key={i} className="navButton p-[12px]" onClick={()=>onClickNav(i)}>{Sections[i]}</button>
+            ))}
+        </div>
+        <div className={
+            DyClass(((width<640)? "flex": "hidden"), "p-[8px] pl-[12px]")
+        }>
+            <button>Menue</button>
+        </div>
+        <div>
+            {drawer}
+        </div>
+    </div>
+
+    </>
     }
 
     export default NavBar;
