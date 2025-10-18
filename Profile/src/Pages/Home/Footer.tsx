db38a1fc211a421676f039a8877ace6bbf7f3d05
Yugen1121 <yugenlimbu777@gmail.com>
2025-10-18 15:05:41 +0100

    Added a footer to the site

---
+++ Profile/src/Pages/Home/Footer.tsx
@@ -0,0 +1,19 @@
+export default function Footer(){
+    return <>
+        <div className="w-full p-15 flex justify-center">
+            <h4>
+                <ul className="list-none flex flex-row gap-4">
+                    <li>
+                        <a href="https://github.com/Yugen1121" target="_blank">Github</a>
+                    </li>
+                    <li>
+                        <a target="_blank">LinkedIn</a>
+                    </li>
+                    <li>
+                        <a target="_blank">Email</a>
+                    </li>
+                </ul>
+            </h4>
+        </div>
+    </>
+}
