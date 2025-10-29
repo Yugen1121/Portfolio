ff2b0bac3f9c071d194776a828ac038e9a7d4905
Yugen1121 <yugenlimbu777@gmail.com>
2025-10-29 11:06:46 +0000

    Changed routes

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
     <>
     
         <Routes>
-          <Route path='/Portfolio' element={<Home />}/>
+          <Route path='/' element={<Home />}/>
           <Route path="/Ripples" element={<Ripples/>} />
         </Routes>
     </>
