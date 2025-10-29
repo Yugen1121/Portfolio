f39c6e07d345cdb5c72f43609157d6a4e0a2d014
Yugen1121 <yugenlimbu777@gmail.com>
2025-10-29 11:27:58 +0000

    Added links and logos

--- src/Pages/Home/Footer.tsx
+++ src/Pages/Home/Footer.tsx
@@ -5,13 +5,20 @@ export default function Footer(){
             <h4>
                 <ul className="list-none flex flex-row gap-4">
                     <li>
-                        <a href="https://github.com/Yugen1121" rel="noopener noreferrer" target="_blank">Github</a>
+                        
+                        <a href="https://github.com/Yugen1121" rel="noopener noreferrer" target="_blank">
+                        <i className="fa fa-github"></i> Github
+                        </a>
                     </li>
                     <li>
-                        <a target="_blank" rel="noopener noreferrer">LinkedIn</a>
+                        <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/yugen-limbu-aa869b33a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app">
+                           <i className="fa fa-linkedin-square"></i> LinkedIn
+                           </a>
                     </li>
                     <li>
-                        <a target="_blank" rel="noopener noreferrer">Email</a>
+                        <a target="_blank" rel="noopener noreferrer" href="mailto:yugenlimbu777@gmail.com">
+                        <i className="fa fa-envelope"></i>
+                        Email</a>
                     </li>
                 </ul>
             </h4>
